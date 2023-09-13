b42a09d850166f0539b388c0a7c10f5b6bd78504
Marixor <m.khoroshiltseva@unive.it>
2023-09-13 17:15:47 +0200

    line_compatibility

--- configs/folder_names.py
+++ configs/folder_names.py
@@ -1,7 +1,7 @@
 ### FOLDERS
-data_path = 'data'
-output_dir = 'output'
-cm_output_name ='compatibility_matrix'
+data_path = '..\\data'
+output_dir = '..\\output'
+cm_output_name = 'compatibility_matrix'
 rm_output_name = 'regions_matrix'
 segm_output_name = 'motif_segmentation'
 lines_segm_name = 'lines'
