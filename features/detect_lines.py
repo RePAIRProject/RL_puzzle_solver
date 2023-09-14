165f1bb1b942f929d25218f815bf4df5f33e6d5c
freerafiki <=>
2023-09-14 18:00:36 +0200

    started working on architecture data

--- features/detect_lines.py
+++ features/detect_lines.py
@@ -13,7 +13,7 @@ import json
 def main(args):
 
     # input
-    images_folder = os.path.join(fnames.data_path, args.puzzle, fnames.imgs_folder)
+    images_folder = os.path.join(fnames.data_path, args.puzzle, 'edge_maps') #fnames.imgs_folder)
     lines_output_folder = os.path.join(fnames.output_dir, args.puzzle, fnames.lines_output_name)
     vis_output = os.path.join(lines_output_folder, 'visualization')
     os.makedirs(vis_output, exist_ok=True)
@@ -48,41 +48,62 @@ def main(args):
             plt.imshow(cv2.cvtColor(rgb_image, cv2.COLOR_BGR2RGB))
             for line2draw in lines_h:
                 plt.axline((line2draw[0], line2draw[1]), (line2draw[2], line2draw[3]))        
-            plt.show()        
-            pdb.set_trace()
+            plt.savefig(os.path.join(vis_output, f"{img_name[:-4]}_hough.jpg"))
+            plt.close()
         
         if method == 'fld':
             # preprocessing
             img = cv2.medianBlur(gray_image, 11)
-            FLD = cv2.ximgproc.createFastLineDetector(length_threshold=cfg.length_threshold)
+            FLD = cv2.ximgproc.createFastLineDetector(length_threshold=cfg.length_threshold) #, distance_threshold=0.5)
             lines_fld = FLD.detect((img).astype(np.uint8)) 
             angles_fld = []
             dists_fld = []
+            p1s_fld = []
+            p2s_fld = []
+            b1s_fld = []
+            b2s_fld = []
+        
             if lines_fld is not None:
                 # convert to polar
                 for line_fld in lines_fld:
                     line_fld = np.squeeze(line_fld[0])
+                    p1 = line_fld[0:2]
+                    p2 = line_fld[2:4]
                     if np.any(line_fld < cfg.border_tolerance) or np.any(line_fld > img.shape[1] - cfg.border_tolerance):
-                        rhofld, thetafld = line_cart2pol(line_fld[0:2], line_fld[2:4])
+                        rhofld, thetafld = line_cart2pol(p1, p2)
                         angles_fld.append(thetafld)
                         dists_fld.append(rhofld)
+                        p1s_fld.append(p1.tolist())
+                        p2s_fld.append(p2.tolist())
+                        if np.any(p1 < cfg.border_tolerance) or np.any(p1 > (img.shape[1] - cfg.border_tolerance)):
+                            b1s_fld.append(0)
+                        else:
+                            b1s_fld.append(1)
+                        if np.any(p2 < cfg.border_tolerance) or np.any(p2 > (img.shape[1] - cfg.border_tolerance)):
+                            b2s_fld.append(0)
+                        else:
+                            b2s_fld.append(1)
 
                 plt.title(f'{img_name}({len(lines_fld)} lines with FLD)')
                 plt.imshow(cv2.cvtColor(rgb_image, cv2.COLOR_BGR2RGB))
                 for line_fld in lines_fld:
                     line2draw = line_fld[0]
                     plt.axline((line2draw[0], line2draw[1]), (line2draw[2], line2draw[3]))        
-                plt.savefig(os.path.join(vis_output, img_name))
+                plt.savefig(os.path.join(vis_output, f"{img_name[:-4]}_em.jpg"))
                 plt.close()
             else:
                 plt.title(f'{img_name} (no lines with FLD)')
                 plt.imshow(cv2.cvtColor(rgb_image, cv2.COLOR_BGR2RGB))    
-                plt.savefig(os.path.join(vis_output, img_name))
+                plt.savefig(os.path.join(vis_output, f"{img_name[:-4]}_em.jpg"))
                 plt.close()
             #pdb.set_trace()
             detected_lines = {
                 'angles': angles_fld,
-                'dists': dists_fld
+                'dists': dists_fld,
+                'p1s': p1s_fld,
+                'p2s': p2s_fld,
+                'b1s': b1s_fld,
+                'b2s': b2s_fld
             }
             with open(os.path.join(lines_output_folder, f"{img_name[:-4]}.json"), 'w') as lj:
                 json.dump(detected_lines, lj, indent=3)
