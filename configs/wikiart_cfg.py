351f1bd9a076da9f2b6b1c748ed63fd8462cd7be
Marixor <m.khoroshiltseva@unive.it>
2023-09-15 15:56:32 +0200

    update

--- configs/wikiart_cfg.py
+++ configs/wikiart_cfg.py
@@ -13,7 +13,7 @@ border_tolerance = 50
 
 ### PREPARATION
 # pieces and grid
-piece_size = 101
+piece_size = 301
 p_hs = piece_size // 2
 xy_grid_points = 3
 theta_grid_points = 4
@@ -23,6 +23,12 @@ canvas_size = pairwise_comp_range + 2 * p_hs + 1
 xy_step = pairwise_comp_range / (comp_matrix_shape[0] - 1)
 theta_step = (360 / comp_matrix_shape[2])
 
+### LINE MATCHING
+rmax = 40
+thr_coef = 0.1
+max_dist = 1000
+
 # preprocess
 num_patches_side = 4
 img_size = num_patches_side*piece_size
+
