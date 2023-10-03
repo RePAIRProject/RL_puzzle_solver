c6a0a3a1abb0666c2931f28118f4702c1a6fe3d8
freerafiki <=>
2023-10-03 16:45:29 +0200

    added support for polylines

--- datasets/create_dataset.py
+++ datasets/create_dataset.py
@@ -30,7 +30,8 @@ if __name__ == '__main__':
     parser.add_argument('-ww', '--width', type=int, default=1920, help='width of the images')
     parser.add_argument('-i', '--imgs_per_line', type=int, default=10, help='number of images for each number of line')
     parser.add_argument('-o', '--output', type=str, default='', help='output folder')
-    parser.add_argument('-t', '--type', type=str, default='segments', choices=['segments', 'lines'], help='choose type of features')
+    parser.add_argument('-t', '--type', type=str, default='segments', choices=['segments', 'lines', 'polylines'], help='choose type of features')
+    parser.add_argument('-th', '--thickness', type=int, default=1, help='thickness of the drawings')
 
     args = parser.parse_args()
     main(args)
--- datasets/geometry.py
+++ datasets/geometry.py
@@ -1,23 +1,27 @@
 import cv2 
 import numpy as np 
 import random 
+import pdb 
 
-def create_random_image(line_type, num_lines, width, height):
+def create_random_image(line_type, num_lines, width, height, lines_length=10, isClosed=False, thickness=1, color=(0,0,0)):
 
     img2draw = np.ones(shape=(width, height), dtype=np.uint8) * 255
-
+    img_shape = [width, height]
     for j in range(num_lines):
         if line_type == 'segments':
-            p1 = generate_random_point([width, height], distribution='uniform')
-            p2 = generate_random_point([width, height], distribution='uniform')
-            img2draw = cv2.line(img2draw, p1, p2, color=(0, 0, 0), thickness=1)
+            p1 = generate_random_point(img_shape, distribution='uniform')
+            p2 = generate_random_point(img_shape, distribution='uniform')
+            img2draw = cv2.line(img2draw, p1, p2, color=color, thickness=thickness)
         elif line_type == 'lines':
             axis1, axis2 = random.sample(range(0,4), 2) # select two axis 
-            p1 = generate_random_point([width, height], distribution='uniform', on_axis=axis1)
-            p2 = generate_random_point([width, height], distribution='uniform', on_axis=axis2)
-            img2draw = cv2.line(img2draw, p1, p2, color=(0, 0, 0), thickness=1)
-        # elif line_type == 'polylines':
-        #     return 1
+            p1 = generate_random_point(img_shape, distribution='uniform', on_axis=axis1)
+            p2 = generate_random_point(img_shape, distribution='uniform', on_axis=axis2)
+            img2draw = cv2.line(img2draw, p1, p2, color=color, thickness=thickness)
+        elif line_type == 'polylines':
+            pts = np.zeros((lines_length, 2), dtype=np.int32)
+            for k in range(lines_length):
+                pts[k, :] = generate_random_point(img_shape, distribution='uniform')
+            img2draw = cv2.polylines(img2draw, [pts], isClosed=isClosed, color=color, thickness=thickness)
 
     return img2draw
 
@@ -39,6 +43,6 @@ def generate_random_point(ranges, distribution='uniform', on_axis=-1):
         p1x = np.random.uniform(0, ranges[0])
         p1y = ranges[1]
 
-    p1 = np.round(np.array([p1x, p1y])).astype(int)
+    p1 = np.round(np.array([p1x, p1y])) #.astype(int)
 
     return p1
