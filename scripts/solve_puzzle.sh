4d0db38c9920dc649ddb0517336e9cfba2112d60
freerafiki <=>
2023-11-06 11:09:40 +0100

    compatibility 16x16 seems to work now

--- scripts/solve_puzzle.sh
+++ scripts/solve_puzzle.sh
@@ -1,13 +1,20 @@
 #!/bin/bash/
 echo "solving $2 from dataset $1 (with $3 pieces)"
 
-echo "compatibility.."
-python compatibility/line_matching_NEW_segments.py --dataset $1 --puzzle $2 --pieces $3 #--penalty $3
+dataset=$1
+puzzle=$2
+pieces=$3
+method=$4
+anchor=27
 
-echo "solver.."
-python solver/solverRotPuzzArgs.py --dataset $1 --puzzle $2 --pieces $3 --anchor $4
-#--penalty $3
+echo "## compatibility.."
+echo "python compatibility/line_matching_NEW_segments.py --dataset $dataset --puzzle $puzzle --pieces $pieces --method $method"
+python compatibility/line_matching_NEW_segments.py --dataset $dataset --puzzle $puzzle --pieces $pieces --method $method
 
-echo "evaluating.."
-python metrics/evaluate.py --dataset $1 --puzzle $2 --num_pieces $3 
+echo "## solver.."
+echo "python solver/solverRotPuzzArgs.py --dataset $dataset --puzzle $puzzle --pieces $pieces --anchor $anchor"
+python solver/solverRotPuzzArgs.py --dataset $dataset --puzzle $puzzle --pieces $pieces --anchor $anchor
 
+echo "## evaluating.."
+echo "python metrics/evaluate.py --dataset $dataset --puzzle $puzzle --num_pieces $pieces "
+python metrics/evaluate.py --dataset $dataset --puzzle $puzzle --num_pieces $pieces 
