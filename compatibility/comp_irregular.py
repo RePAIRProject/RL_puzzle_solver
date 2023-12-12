fixing bugs on rotation

compatibility/comp_irregular.py
@@ -10,8 +10,7 @@ from configs import folder_names as fnames
 from puzzle_utils.shape_utils import prepare_pieces_v2, create_grid, include_shape_info
 from puzzle_utils.pieces_utils import calc_parameters
 from puzzle_utils.visualization import save_vis
-from puzzle_utils.lines_ops import compute_cost_matrix_LAP
-from configs import line_matching_cfg as cfg
+from puzzle_utils.lines_ops import compute_cost_matrix_LAP, calc_line_matching_parameters
 
 def reshape_list2mat_and_normalize(comp_as_list, n, norm_value):
     first_element = comp_as_list[0]
@@ -21,7 +20,7 @@ def reshape_list2mat_and_normalize(comp_as_list, n, norm_value):
         for j in range(n):
             ji_mat = comp_as_list[i*n + j]
             cost_matrix[:,:,:,j,i] = ji_mat
-            norm_cost_matrix[:,:,:,j,i] = np.maximum(1 - ji_mat / cfg.rmax, 0)
+            norm_cost_matrix[:,:,:,j,i] = np.maximum(1 - ji_mat / norm_value, 0)
     return cost_matrix, norm_cost_matrix
 
 def main(args):
@@ -56,6 +55,8 @@ def main(args):
         else:
             ppars = calc_parameters(img_parameters)
 
+        line_matching_parameters = calc_line_matching_parameters(ppars)
+
         pieces = include_shape_info(fnames, pieces, args.dataset, puzzle, args.method)
 
         region_mask_mat = scipy.io.loadmat(os.path.join(os.getcwd(), fnames.output_dir, args.dataset, puzzle, fnames.rm_output_name, f'RM_{puzzle}.mat'))
@@ -72,7 +73,7 @@ def main(args):
         z_id = m * z_rad
         ang = ppars.theta_step
         rot = np.arange(0, 360 - ang + 1, ang)
-        cmp_parameters = (p, z_id, m, rot, cfg)
+        cmp_parameters = (p, z_id, m, rot, line_matching_parameters)
         n = len(pieces)
 
         # COST MATRICES 
@@ -87,13 +88,13 @@ def main(args):
             #with parallel_backend('threading', n_jobs=args.jobs):
             costs_list = Parallel(n_jobs=args.jobs, prefer="threads")(delayed(compute_cost_matrix_LAP)(i, j, pieces, region_mask, cmp_parameters, ppars) for j in range(n) for i in range(n))
             #costs_list = Parallel(n_jobs=args.jobs)(delayed(compute_cost_matrix_LAP)(i, j, pieces, region_mask, cmp_parameters, ppars) for j in range(n) for i in range(n))
-            All_cost, All_norm_cost = reshape_list2mat_and_normalize(costs_list, n=n, norm_value=cfg.rmax)
+            All_cost, All_norm_cost = reshape_list2mat_and_normalize(costs_list, n=n, norm_value=line_matching_parameters.rmax)
         else:
             for i in range(n):  # select fixed fragment
                 for j in range(n):
                     ji_mat = compute_cost_matrix_LAP(i, j, pieces, region_mask, cmp_parameters, ppars, verbose=True)
                     All_cost[:, :, :, j, i] = ji_mat
-                    All_norm_cost[:,:,:,j,i] = np.maximum(1 - ji_mat / cfg.rmax, 0)
+                    All_norm_cost[:,:,:,j,i] = np.maximum(1 - ji_mat / line_matching_parameters.rmax, 0)
 
         # apply region masks
         R_line = (All_norm_cost * region_mask) * 2
@@ -105,13 +106,13 @@ def main(args):
         # save output
         output_folder = os.path.join(fnames.output_dir, args.dataset, args.puzzle, fnames.cm_output_name)
         os.makedirs(output_folder, exist_ok=True)
-        filename = os.path.join(output_folder, f'CM_lines_{args.method}_p{cfg.mismatch_penalty}')
+        filename = os.path.join(output_folder, f'CM_lines_{args.method}_p{line_matching_parameters.mismatch_penalty}')
         mdic = {"R_line": R_line, "label": "label"}
         scipy.io.savemat(f'{filename}.mat', mdic)
         np.save(filename, R_line)
 
         if args.save_everything is True:
-            filename = os.path.join(output_folder, f'CM_cost_{args.method}_p{cfg.mismatch_penalty}')
+            filename = os.path.join(output_folder, f'CM_cost_{args.method}_p{line_matching_parameters.mismatch_penalty}')
             mdic = {"All_cost": All_cost, "label": "label"}
             scipy.io.savemat(f'{filename}.mat', mdic)
             np.save(filename, All_cost)

features/compute_both_regions_masks.py
@@ -67,13 +67,8 @@ def main(args):
         print('#' * 50)
         print()
 
-        ## CREATE MATRIX
-        debug = False
-        if debug is True:
-            plt.ion()
-                      
+        ## CREATE MATRIX                      
         RM_combo = np.zeros((grid_size_xy, grid_size_xy, grid_size_rot, len(pieces), len(pieces)))
-        #RM_big = np.zeros((3001, 3001, grid_size_rot, len(pieces), len(pieces)))
         RM_lines = np.zeros((grid_size_xy, grid_size_xy, grid_size_rot, len(pieces), len(pieces)))
         RM_shapes = np.zeros((grid_size_xy, grid_size_xy, grid_size_rot, len(pieces), len(pieces)))
         for i in range(len(pieces)):
@@ -112,45 +107,10 @@ def main(args):
                         resized_combo = cv2.resize(converted_combo, (ppars.comp_matrix_shape[0], ppars.comp_matrix_shape[1]), cv2.INTER_NEAREST)
 
                         # These are the matrices
-                        RM_combo[:,:,t,i,j] = (resized_combo.astype(np.int32) - 1)
-                        RM_lines[:,:,t,i,j] = cv2.resize(binary_overlap_lines.astype(np.uint8), (ppars.comp_matrix_shape[0], ppars.comp_matrix_shape[1]))
-                        RM_shapes[:,:,t,i,j] = (resized_shape.astype(np.int32) - 1)
+                        RM_combo[:,:,t,j,i] = (resized_combo.astype(np.int32) - 1)
+                        RM_lines[:,:,t,j,i] = cv2.resize(binary_overlap_lines.astype(np.uint8), (ppars.comp_matrix_shape[0], ppars.comp_matrix_shape[1]))
+                        RM_shapes[:,:,t,j,i] = (resized_shape.astype(np.int32) - 1)
                         
-                    #####################################################
-                    #####################################################
-                    #####################################################
-                    if debug is True:
-                        #plt.figure(figsize=(32,32))
-                        plt.subplot(251)
-                        plt.title(f"mask {i}")
-                        plt.imshow(piece_i_on_canvas['mask'])
-                        plt.subplot(256)
-                        plt.title(f"mask {j}")
-                        plt.imshow(piece_j_on_canvas['mask'])
-                        xshift = 736    # 702
-                        yshift = -108   #  25
-                        debug_angles = [0, 90, 180, 270]
-                        for k, ang in enumerate(debug_angles):
-                            plt.subplot(2, 5, 2 + k)
-                            plt.title(f"{i}vs{j} at ({center_pos+yshift}, {center_pos+xshift}, {ang})")
-                            two_shapes = np.zeros((ppars.canvas_size, ppars.canvas_size))
-                            two_shapes += piece_i_on_canvas['mask']
-                            piece_j_hyp = place_on_canvas(pieces[j], (center_pos+yshift, center_pos+xshift), ppars.canvas_size, ang)
-                            two_shapes += 2*piece_j_hyp['mask']
-                            plt.imshow(two_shapes)
-                            plt.subplot(2, 5, 7 + k)
-                            
-                            ccrm = [(center_pos+yshift) // 30, (center_pos+xshift) // 30]
-                            rm_rot = RM_big[:,:,ang//90,i,j]
-                            val = rm_rot[ccrm[0], ccrm[1]]
-                            plt.title(f'region map ({ccrm[0], ccrm[1]}) = {val}')
-                            rm_rot[ccrm[0], ccrm[1]] = 2
-                            rm_rot[ccrm[1], ccrm[0]] = 3
-                            plt.imshow(rm_rot)
-                        pdb.set_trace()
-                    #####################################################
-                    #####################################################
-                    #####################################################
 
         print("\n")
         print('Done calculating')

puzzle_utils/lines_ops.py
@@ -12,6 +12,17 @@ from scipy.optimize import linear_sum_assignment
 #from itertools import compress
 import time 
 
+class CfgParameters(dict):
+    __getattr__ = dict.__getitem__
+
+def calc_line_matching_parameters(parameters):
+    lm_pars = CfgParameters()
+    lm_pars['thr_coef'] = 0.08
+    lm_pars['max_dist'] = parameters.xy_step 
+    lm_pars['badmatch_penalty'] = lm_pars['max_dist'] * 5 / 3 # parameters.piece_size / 3 #?
+    lm_pars['mismatch_penalty'] = lm_pars['max_dist'] * 4 / 3 # parameters.piece_size / 4 #?
+    lm_pars['rmax'] = lm_pars['max_dist'] * 7 / 6
+    return lm_pars
 
 def draw_lines(lines_dict, img_shape):
     angles, dists, p1s, p2s = extract_from(lines_dict)
@@ -65,7 +76,8 @@ def line_poligon_intersect(z_p, theta_p, poly_p, z_l, theta_l, s1, s2, pars):
     return intersections, np.array(useful_lines_s1), np.array(useful_lines_s2)
 
 
-def compute_cost_matrix(p, z_id, m, rot, alfa1, alfa2, r1, r2, s11, s12, s21, s22, poly1, poly2, cfg, mask_ij, pars):
+def compute_cost_matrix(p, z_id, m, rot, alfa1, alfa2, r1, r2, s11, s12, s21, s22, poly1, poly2, lmp, mask_ij, pars):
+    # lmp is the old cfg (with the parameters)
     R_cost = np.zeros((m.shape[1], m.shape[1], len(rot)))
 
     #for t in range(1):
@@ -84,7 +96,7 @@ def compute_cost_matrix(p, z_id, m, rot, alfa1, alfa2, r1, r2, s11, s12, s21, s2
                     #print([iy, ix, t])
 
                     # check if line1 crosses the polygon2                  
-                    intersections1, useful_lines_s11, useful_lines_s12 = line_poligon_intersect(z, theta, poly2, [0, 0], 0, s11, s12, pars)
+                    intersections1, useful_lines_s11, useful_lines_s12 = line_poligon_intersect(z, -theta, poly2, [0, 0], 0, s11, s12, pars)
 
                     # return intersections                    
                     useful_lines_alfa1 = alfa1[intersections1] # no rotation here!
@@ -92,9 +104,9 @@ def compute_cost_matrix(p, z_id, m, rot, alfa1, alfa2, r1, r2, s11, s12, s21, s2
                     useful_lines_s12 = useful_lines_s12[intersections1]
 
                     # check if line2 crosses the polygon1
-                    intersections2, useful_lines_s21, useful_lines_s22 = line_poligon_intersect([0, 0], 0, poly1, z, theta, s21, s22, pars)
+                    intersections2, useful_lines_s21, useful_lines_s22 = line_poligon_intersect([0, 0], 0, poly1, z, -theta, s21, s22, pars)
 
-                    useful_lines_alfa2 = alfa2[intersections2] - theta_rad # the rotation!
+                    useful_lines_alfa2 = alfa2[intersections2] + theta_rad # the rotation!
                     useful_lines_s21 = useful_lines_s21[intersections2]
                     useful_lines_s22 = useful_lines_s22[intersections2]
 
@@ -102,11 +114,11 @@ def compute_cost_matrix(p, z_id, m, rot, alfa1, alfa2, r1, r2, s11, s12, s21, s2
                     n_lines_f2 = useful_lines_alfa2.shape[0]
 
                     if n_lines_f1 == 0 and n_lines_f2 == 0:
-                        tot_cost = cfg.max_dist * 2  # accept with some cost
+                        tot_cost = lmp.max_dist * 2  # accept with some cost
 
                     elif (n_lines_f1 == 0 and n_lines_f2 > 0) or (n_lines_f1 > 0 and n_lines_f2 == 0):
                         n_lines = (np.max([n_lines_f1, n_lines_f2]))
-                        tot_cost = cfg.mismatch_penalty * n_lines
+                        tot_cost = lmp.mismatch_penalty * n_lines
 
                     else:
                         # Compute cost_matrix, LAP, penalty, normalize
@@ -124,11 +136,11 @@ def compute_cost_matrix(p, z_id, m, rot, alfa1, alfa2, r1, r2, s11, s12, s21, s2
                                 d3 = distance.euclidean(useful_lines_s12[i], useful_lines_s21[j])
                                 d4 = distance.euclidean(useful_lines_s12[i], useful_lines_s22[j])
 
-                                dist_matrix0[i, j] = np.min([d1, d2, d3, d4])
+                                dist_matrix[i, j] = np.min([d1, d2, d3, d4])
 
-                        dist_matrix[gamma_matrix > cfg.thr_coef] = cfg.badmatch_penalty
-                        dist_matrix[dist_matrix0 > cfg.max_dist] = cfg.badmatch_penalty
-                        # dist_matrix[dist_matrix0 > cfg.badmatch_penalty] = cfg.badmatch_penalty
+                        dist_matrix[gamma_matrix > lmp.thr_coef] = lmp.badmatch_penalty
+                        dist_matrix[dist_matrix > lmp.max_dist] = lmp.badmatch_penalty
+                        # dist_matrix[dist_matrix0 > lmp.badmatch_penalty] = lmp.badmatch_penalty
 
                         # # LAP
                         row_ind, col_ind = linear_sum_assignment(dist_matrix)
@@ -136,7 +148,7 @@ def compute_cost_matrix(p, z_id, m, rot, alfa1, alfa2, r1, r2, s11, s12, s21, s2
                         #print([tot_cost])
 
                         # # penalty
-                        penalty = np.abs(n_lines_f1 - n_lines_f2) * cfg.mismatch_penalty  # no matches penalty
+                        penalty = np.abs(n_lines_f1 - n_lines_f2) * lmp.mismatch_penalty  # no matches penalty
                         tot_cost = (tot_cost + penalty)
                         tot_cost = tot_cost / np.max([n_lines_f1, n_lines_f2])  # normalize to all lines in the game
 
@@ -153,7 +165,7 @@ def compute_cost_matrix_LAP(idx1, idx2, pieces, regions_mask, cmp_parameters, pp
     Wrapper for the cost computation, so that it can be called in one-line, making it easier to parallelize using joblib's Parallel (in comp_irregular.py) 
     """
 
-    (p, z_id, m, rot, cfg) = cmp_parameters
+    (p, z_id, m, rot, line_matching_pars) = cmp_parameters
     n = len(pieces)
     
     if verbose is True:
@@ -168,10 +180,10 @@ def compute_cost_matrix_LAP(idx1, idx2, pieces, regions_mask, cmp_parameters, pp
 
         if len(alfa1) == 0 and len(alfa2) == 0:
             #print('no lines')
-            R_cost = np.zeros((m.shape[1], m.shape[1], len(rot))) + cfg.max_dist * 2
+            R_cost = np.zeros((m.shape[1], m.shape[1], len(rot))) + line_matching_pars.max_dist * 2
         elif (len(alfa1) > 0 and len(alfa2) == 0) or (len(alfa1) == 0 and len(alfa2) > 0):
             #print('only one side with lines')
-            R_cost = np.zeros((m.shape[1], m.shape[1], len(rot))) + cfg.mismatch_penalty
+            R_cost = np.zeros((m.shape[1], m.shape[1], len(rot))) + line_matching_pars.mismatch_penalty
         else:
             #print('values!')
             poly1 = pieces[idx1]['polygon']
@@ -180,30 +192,11 @@ def compute_cost_matrix_LAP(idx1, idx2, pieces, regions_mask, cmp_parameters, pp
             candidate_values = np.sum(mask_ij)
             t1 = time.time()
             R_cost = compute_cost_matrix(p, z_id, m, rot, alfa1, alfa2, r1, r2, s11,
-                                        s12, s21, s22, poly1, poly2, cfg, mask_ij, ppars)
+                                        s12, s21, s22, poly1, poly2, line_matching_pars, mask_ij, ppars)
             if verbose is True:
                 print(f"cost matrix piece {idx1} ({len(alfa1)} lines) vs piece {idx2} ({len(alfa2)} lines): took {(time.time()-t1):.02f} seconds ({candidate_values:.1f} values) ")
             #print(R_cost)
     return R_cost
-
-# def polar_cartesian_check():
-# print("check polar/cart")
-# print("polar")
-# print(angles, dists)
-# print('\n')
-# cartesians = polar2cartesian(seg_img, angles, dists)
-# print('cartesian')
-# print(cartesians)
-# print('\n\n')
-# for j, cartline in enumerate(cartesians):
-#     #cartline = line_pol2cart(angle, dist)
-#     print(f'\noriginal polar: theta={angles[j]:.3f}, rho={dists[j]:.3f}')
-#     print(f'converted to cartesian: pt1 = ({cartline[0]:.3f}, {cartline[1]:.3f}), pt2 = ({cartline[2]:.3f}, {cartline[3]:.3f})')
-#     rho, theta = line_cart2pol(cartline[0:2], cartline[2:4])
-#     print(f'polar: theta={theta:.3f}, rho={rho:.3f}')
-
-# print('\n\n')
-# pdb.set_trace()
 
 # Convert polar coordinates to Cartesian coordinates and display the result
 def polar2cartesian(image, angles, dists, show_image=False):