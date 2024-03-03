9909c5235f3279bbfc7de2e713d44fd1881163ef
freerafiki <bagigi@disroot.org>
2024-03-03 21:57:55 +0100

    writing neighbours to json and txt

--- metrics/write_neighbours_list.py
+++ metrics/write_neighbours_list.py
@@ -56,11 +56,18 @@ def main(args):
         print(neighbours_nums)
         print("Neighbours as matrix")
         print(neighbours_matrix)
+        neigh_dict = {
+            'names': neighbours_names,
+            'numbers': neighbours_nums
+        }
+        with open(os.path.join(puzzle_folder, 'neighbours.json'), 'w') as ej:
+            json.dump(neigh_dict, ej, indent=3)
+        np.savetxt(os.path.join(puzzle_folder, 'neighbours.txt'), neighbours_matrix, fmt='%.0f')
             
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='........ ')  # add some discription
+    parser = argparse.ArgumentParser(description='Writes a file with the neighbors (reading from regions folder)')  # add some discription
     parser.add_argument('-d', '--dataset', type=str, default='manual_lines', help='dataset folder')   
     parser.add_argument('--verbosity', type=int, default=1, help='level of logging/printing (0 --> nothing, higher --> more printed stuff)')                 # repair_g28, aki-kuroda_night-2011, pablo_picasso_still_life
     parser.add_argument('-p', '--puzzle', type=str, default='', help='puzzle folder')    
