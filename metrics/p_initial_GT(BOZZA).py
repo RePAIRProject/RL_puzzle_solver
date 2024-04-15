5a0895b9a7e32d8804f7e2824ea7108296f104b3
Marixor <m.khoroshiltseva@unive.it>
2024-04-15 12:39:58 +0200

    initialization from GT

--- metrics/p_initial_GT(BOZZA).py
+++ metrics/p_initial_GT(BOZZA).py
@@ -12,6 +12,8 @@ import cv2
 
 
 def main(args):
+# def initialization_from_GT(R, args.dataset, args.puzzle, args.anchor, args.p_pts)
+
     dataset_folder = os.path.join(os.getcwd(), fnames.output_dir, args.dataset)
     print(dataset_folder)
     if args.puzzle == '':
@@ -20,12 +22,12 @@ def main(args):
     else:
         puzzles = [args.puzzle]
 
-    puzzle_folder = os.path.join(dataset_folder, puzzle)
-    general_files = os.listdir(puzzle_folder)
-    # solution_folders = [sol_fld for sol_fld in general_files if "solution" in sol_fld]
-
     print(f"\nEvaluate solution for: {puzzles}\n")
+
     for puzzle in puzzles:
+        puzzle_folder = os.path.join(dataset_folder, puzzle)
+        general_files = os.listdir(puzzle_folder)
+        # solution_folders = [sol_fld for sol_fld in general_files if "solution" in sol_fld]
         with open(os.path.join(puzzle_folder, "ground_truth.json"), 'r') as gtj:
             ground_truth = json.load(gtj)
         with open(os.path.join(puzzle_folder, f"parameters_{puzzle}.json"), 'r') as gtj:
@@ -36,8 +38,6 @@ def main(args):
         anchor_id = 5
         no_rotations = 4
         theta_step = 360 / no_rotations
-        anc_coord = gt_coord[anchor_id, 0:2]
-        anc_rot = gt_coord[anchor_id, 2]
 
         num_pcs = img_parameters['num_pieces']
         gt_coord = np.zeros([num_pcs, 3])
@@ -45,38 +45,50 @@ def main(args):
             gt_coord[j, 2] = (+1) * ground_truth[f"piece_{j:04d}"]['rotation']
             gt_coord[j, 0:2] = (-1) * np.asarray(ground_truth[f"piece_{j:04d}"]['translation'][::-1])
 
-            # GT - shifted and rotated in PX (anchor is in ref.point = [0,0,0])
-            gt_shift = np.zeros([num_pcs, 3])
-            gt_shift[:, 0:2] = gt_coord[:, 0:2] - anc_coord
-            gt_shift[:, 2] = gt_coord[:, 2] - anc_rot
-            # print(f"GT non rotated in px :")
-            # print(gt_shift)
+        # GT - shifted and rotated in PX (anchor is in ref.point = [0,0,0])
+        anc_coord = gt_coord[anchor_id, 0:2]
+        anc_rot = gt_coord[anchor_id, 2]
 
-            # 2. Translate to yxz space
-            gt_yxz = np.zeros([num_pcs, 3])
-            gt_yxz[:, 0:2] = np.round(gt_shift[:, 0:2] / cmp_parameters['xy_step'])
-            gt_yxz[:, 2] = gt_shift[:, 2] / theta_step
-            # print(f"GT non rotated in YX :")
-            # print(gt_yxz)
+        # 1. Shift GT coord according to anchor position = [0,0,0]
+        gt_shift = np.zeros([num_pcs, 3])
+        gt_shift[:, 0:2] = gt_coord[:, 0:2] - anc_coord
+        gt_shift[:, 2] = gt_coord[:, 2] - anc_rot
+        # print(f"GT non rotated in px :")
+        # print(gt_shift)
 
-            # 3. Rotate GT according anchor rotation - CHECK!!!
-            gt_rot = np.zeros([num_pcs, 3])
-            if anc_rot == 0:
-                gt_rot[:, 0:2] = gt_yxz[:, 0:2]
-            else:
-                if anc_rot == 90 or anc_rot == -270:
-                    gt_rot[:, 0] = gt_yxz[:, 1]  # y_new = +x
-                    gt_rot[:, 1] = -gt_yxz[:, 0]  # x_new = -y
-                elif anc_rot == -90 or anc_rot == 270:
-                    gt_rot[:, 0] = -gt_yxz[:, 1]  # y_new = -x
-                    gt_rot[:, 1] = gt_yxz[:, 0]  # x_new = +y
-                elif anc_rot == 180 or anc_rot == -180:
-                    gt_rot[:, 0] = -gt_yxz[:, 0]  # y_new = -x
-                    gt_rot[:, 1] = -gt_yxz[:, 1]  # x_new = -y
-            gt_rot[:, 2] = gt_yxz[:, 2]
-            print(f"  ")
-            print(f"GT shifted and rotated in YX :")
-            print(gt_rot)
+        # 2. Translate to yxz space
+        gt_yxz = np.zeros([num_pcs, 3])
+        gt_yxz[:, 0:2] = np.round(gt_shift[:, 0:2] / cmp_parameters['xy_step'])
+        gt_yxz[:, 2] = gt_shift[:, 2] / theta_step
+        # print(f"GT non rotated in YX :")
+        # print(gt_yxz)
+
+        # 3. Rotate GT according anchor rotation - CHECK!!!
+        gt_rot = np.zeros([num_pcs, 3])
+        gt_rot[:, 2] = gt_yxz[:, 2]
+
+        if anc_rot == 0:
+            gt_rot[:, 0:2] = gt_yxz[:, 0:2]
+        else:
+            if anc_rot == 90 or anc_rot == -270:
+                gt_rot[:, 0] = gt_yxz[:, 1]  # y_new = +x
+                gt_rot[:, 1] = -gt_yxz[:, 0]  # x_new = -y
+            elif anc_rot == -90 or anc_rot == 270:
+                gt_rot[:, 0] = -gt_yxz[:, 1]  # y_new = -x
+                gt_rot[:, 1] = gt_yxz[:, 0]  # x_new = +y##
+            elif anc_rot == 180 or anc_rot == -180:
+                gt_rot[:, 0] = -gt_yxz[:, 0]  # y_new = -x
+                gt_rot[:, 1] = -gt_yxz[:, 1]  # x_new = -y
+
+        # Shift ALL coord to the center of the Reconstruction plane !!!!
+        x0 = round(args.p_pts / 2)
+        y0 = round(args.p_pts / 2)
+
+
+
+        print(f'  ')
+        print(f"GT shifted and rotated in YX :")
+        print(gt_rot)
 
     print("#" * 50)
     print("FINISHED")
