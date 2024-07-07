18f741f7de83173fc7007acf4c8159eb81cf9cc1
freerafiki <bagigi@disroot.org>
2024-07-07 17:16:41 +0200

    cmap heatmaps

--- metrics/heatmap_anchors.py
+++ metrics/heatmap_anchors.py
@@ -4,7 +4,8 @@ import numpy as np
 import json 
 import seaborn as sns
 
-folder = '/media/lucap/big_data/datasets/repair/eccv_exp/quantitative_evaluation'
+#folder = '/media/lucap/big_data/datasets/repair/eccv_exp/quantitative_evaluation'
+folder = 'output/ACCV24/maps_quantitative_evaluation'
 
 heatmap_D = np.zeros((8,8))
 heatmap_N = np.zeros((8,8))
@@ -17,11 +18,13 @@ for i in range(64):
     heatmap_N[i % 8, i // 8] = eval_i['neighbours']
 
 plt.figure(figsize=(32,16))
-plt.suptitle("Image 00003 (Barcelona), Squared Pieces")
+plt.suptitle("Image 00003 (Barcelona), 64 Squared Pieces", fontsize=26)
 plt.subplot(121)
-plt.title("Direct Metric")
-sns.heatmap(heatmap_D, annot=True)
+plt.title("Direct Metric", fontsize=22)
+sns.heatmap(heatmap_D, annot=True, square=True, cbar=False, cmap='RdYlGn', vmin=0)
+#plt.axis('equal')
 plt.subplot(122)
-plt.title("Neighbours Metric")
-sns.heatmap(heatmap_N, annot=True)
+plt.title("Neighbours Metric", fontsize=22)
+sns.heatmap(heatmap_N, annot=True, square=True, cbar=False, cmap='RdYlGn', vmin=0)
+#plt.axis('equal')
 plt.show()
