9651359a2130b7758ce7e69340bbbc0b3dcb47b6
Marco Ronchese <8733504+emarj@users.noreply.github.com>
2025-06-17 17:05:30 +0200

    added PIL reconstruction, the other needs to be checked

--- new_code/utils/visualization_utils.py
+++ new_code/utils/visualization_utils.py
@@ -4,7 +4,9 @@ import cv2
 from typing import List, Optional
 from utils.puzzle_utils import PuzzlePiece
 import matplotlib as mpl
-from scipy.ndimage import rotate
+import scipy
+from PIL import Image
+import os
 
 def save_compatibility_matrix_visualization_to_file(CM, pieces: List[PuzzlePiece], rot_step, output_folder, title='', draw_figsize=(100, 100), all_rotation=False, save_every=6, img_format='jpg', vmin=-1, vmax=1):
     
@@ -58,12 +60,12 @@ def reconstruct(pixel_solution,
     
     piece_size = pieces[0].data.image.shape
 
+    if piece_size[0] != piece_size[1]:
+        raise Exception('only square images of pieces are supported')
+
     # half piece sizes
     hps_x = piece_size[0]//2
     hps_y = piece_size[1]//2
-
-    if hps_x != hps_y:
-        raise Exception('only square images of pieces are supported')
     
     offset = 1 if hps_x * 2 < piece_size[0] else 0
 
@@ -73,29 +75,100 @@ def reconstruct(pixel_solution,
     
     image = np.zeros(dimension + (4,)) # (dimension[0], dimension[1], 4)
 
-    if show_borders == True:
-        borders_cmap = mpl.colormaps[colormap_name].resampled(len(pieces))
+    # if show_borders == True:
+    #     borders_cmap = mpl.colormaps[colormap_name].resampled(len(pieces))
 
     for i in range(len(pieces)):
         # TODO: confidence should be optional
         x, y, theta, confidence = pixel_solution[i]
 
-        if confidence > confidence_threshold:
-            rotated_piece_img = rotate(pieces[i].data.image, theta, reshape=False, mode='constant', order=0)
+        if confidence <= confidence_threshold:
+            continue
 
+        piece_img = pieces[i].data.image
+        if theta != 0:
+            piece_img = scipy.ndimage.rotate(piece_img, theta, reshape=False, mode='constant', order=0)
 
-            image[x - hps_x:x + hps_x + offset, y - hps_y:y + hps_y + offset,:] += rotated_piece_img
+        image[x - hps_x:x + hps_x + offset, y - hps_y:y + hps_y + offset,:] += piece_img
 
-            # if show_borders == True:
-            #         mask = (Im > 0.05).astype(np.uint8)
-            #         em = cv2.erode(mask, np.ones((5, 5)))
-            #         bordered_im = Im * em + (mask - em) * borders_cmap(i)[:3]
-            #         Im = bordered_im
+        # if show_borders == True:
+        #         mask = (Im > 0.05).astype(np.uint8)
+        #         em = cv2.erode(mask, np.ones((5, 5)))
+        #         bordered_im = Im * em + (mask - em) * borders_cmap(i)[:3]
+        #         Im = bordered_im
            
     return image
 
-def save_visual_reconstruction_to_file(solution: List):
-    pass
+def reconstruct_pil(
+                solution,
+                pieces: List[PuzzlePiece],
+                dimension: Optional[tuple[int, int]] = None,
+                confidence_threshold : float = 0,
+                expand_on_rotate : bool = False,
+                crop_to_content : bool = True,
+                show_borders : bool = True,
+                colormap_name : str = 'jet') -> np.ndarray:
+
+    piece_size = pieces[0].data.image.shape
+
+    if piece_size[0] != piece_size[1]:
+        raise Exception('only square images of pieces are supported')
+    
+    if dimension is None:
+        # TODO calculate best dimensions from solution
+        dimension = (len(pieces) * piece_size[0], len(pieces) * piece_size[1])
+    
+    canvas = np.zeros(dimension + (4,), dtype=np.uint8) # (dimension[0], dimension[1], 4)
+    canvas = Image.fromarray(canvas, mode="RGBA")
+
+    for i in range(len(pieces)):
+        # TODO: confidence should be optional
+        #x, y, theta, confidence = pixel_solution[i]
+        x, y, theta, confidence = map(float, solution[i])
+
+        if confidence <= confidence_threshold:
+            continue
+
+        piece_img = pieces[i].data.image
+
+        if piece_img.dtype in (np.float32, np.float64):
+            piece_img = np.clip(piece_img, 0.0, 1.0) * 255
+            piece_img = piece_img.astype(np.uint8)
+
+        piece_img = Image.fromarray(piece_img, mode="RGBA")
+
+        plt.imshow(piece_img)
+        plt.show()
+
+        if theta != 0:
+            piece_img = piece_img.rotate(theta, expand=expand_on_rotate, fillcolor=(0,0,0,0))
+
+        pos = (int(x - piece_img.width // 2), int(y - piece_img.height // 2))
+        canvas.paste(piece_img, pos, mask=piece_img)  # Use the alpha channel as mask
+
+
+    if crop_to_content:
+        canvas = crop_to_content_pil(canvas)
+
+    return canvas
+
+def crop_to_content_pil(img: Image.Image, padding: int = 1) -> Image.Image:
+    assert img.mode == "RGBA", "Image must be in RGBA mode"
+
+    alpha = img.split()[3]
+    bbox = alpha.getbbox()
+
+    if not bbox:
+        return img  # fully transparent, nothing to crop
+
+    # Expand bbox by padding, making sure we stay within image bounds
+    left = max(bbox[0] - padding, 0)
+    upper = max(bbox[1] - padding, 0)
+    right = min(bbox[2] + padding, img.width)
+    lower = min(bbox[3] + padding, img.height)
+
+    padded_bbox = (left, upper, right, lower)
+    return img.crop(padded_bbox)
 
 
 def crop_to_content(image:np.ndarray, padding:int=1, return_vals:bool=False, max_noise:int=0):
@@ -111,7 +184,7 @@ def crop_to_content(image:np.ndarray, padding:int=1, return_vals:bool=False, max
         y0 = np.min(np.where(image > max_noise)[0]) - padding
         y1 = np.max(np.where(image > max_noise)[0]) + padding
 
-    if return_vals == True:
+    if return_vals:
         return image[y0:y1, x0:x1, :], x0, x1, y0, y1
     return image[y0:y1, x0:x1, :]
 
